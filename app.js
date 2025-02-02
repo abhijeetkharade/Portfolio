41a15b489539ed7b8925c35bf817bafdbd0c7286
abhijeetkharade <abhijeetkharade2004@gmail.com>
2025-02-02 23:41:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let linkedin = document.querySelector("#box1");
+
+linkedin.addEventListener("click",()=>{
+    window.open("https://www.linkedin.com/in/abhijeetkharade");
+}) 
+
+let github = document.querySelector("#box2");
+
+github.addEventListener("click",()=>{
+    window.open("https://github.com/sanketkharade2004");
+}) 
